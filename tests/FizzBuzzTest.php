d8e059e93fb01d4237f2a3829dd8980437b8b8de
Viviana <vivitr5195@gmail.com>
2022-08-16 09:03:43 +0200

    init repo

---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests;
+
+use App\FizzBuzz;
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzTest extends TestCase {
+    public function test_is_divisible_by_3_and_return_Fizz()
+    {
+        $fizzBuzz = new FizzBuzz; //given
+        $result = $fizzBuzz -> isDivisible(3);
+$this->assertEquals("Fizz",$result);
+    }
+    public function test_is_divisible_by_5_and_return_Buzz()
+    {
+        $fizzBuzz = new FizzBuzz; //given
+        $result = $fizzBuzz -> isDivisible(5);
+$this->assertEquals("Buzz",$result);
+    }
+    public function test_is_divisible_by_5_and_return_FizzBuzz()
+    {
+        $fizzBuzz = new FizzBuzz; //given
+        $result = $fizzBuzz -> isDivisible(15);
+$this->assertEquals("FizzBuzz",$result);
+    }
+    public function test_FizzBuzz()
+    {
+        
+        $fizzBuzz = new FizzBuzz; //given
+        $numb = 7;
+        $result = $fizzBuzz -> isDivisible($numb);
+$this->assertEquals($numb,$result);
+    }
+    public function test_is_divisible_by_3_and_have_return_Fizz()
+    {
+        $fizzBuzz = new FizzBuzz; //given
+        $result = $fizzBuzz -> isDivisible3(534);
+$this->assertEquals("Fizz",$result);
+    }    
+}
